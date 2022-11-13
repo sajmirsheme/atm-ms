9d9039adb3eb30744e932a1b5f13ba9089b9fad5
Sindi <sinditaipi15@gmail.com>
2022-11-13 14:52:06 +0100

    AccountRepository added in repository package

--- src/Main/Java/org/example/repository/AccountRepository.java
+++ src/Main/Java/org/example/repository/AccountRepository.java
@@ -1,5 +1,20 @@
 package org.example.repository;
 
+import org.example.model.Account;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class AccountRepository {
 
+    public void insert(Account account) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("atmsystemdb-em");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        entityManager.getTransaction().begin();
+
+        entityManager.persist(account);
+
+    }
 }
